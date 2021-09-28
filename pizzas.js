90874e8e374a06fab11f0e37f4bdb73a0f7e1f1a
Lucas Henrique da Silva Araujo <90270499+lucas-henrique26@users.noreply.github.com>
2021-09-28 15:24:58 -0300

    commit inicial

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:18.00, sizes:['100g', '530g', '860g'], description:'A Pizza de Mussarela é coberta com molho de tomate, queijo tipo mussarela, azeitonas pretas e orégano e massa com fermentação natural, oferece mais sabor e qualidade à sua mesa.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:20.19, sizes:['320g', '530g', '860g'], description:'Uma deliciosa combinação de Linguiça Calabresa, rodelas de cebolas frescas, azeitonas pretas, mussarela, polpa de tomate, orégano e massa especial garante um sabor único à Pizza de Calabresa.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Os queijos dessa pizza são mussarela, catupiry, provolone, gorgonzola e parmesão. Eles combinam tão bem.'},
+    {id:4, name:'Brócolis', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, azeitona e brócolis. Um verdadeiro clássico. Essa opção traz a combinação de sabores que todo mundo ama e se lembra imediatamente quando pensa em pizza.'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Deliciosa pizza de sorvete napolitano. Sabor sem igual.'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Especialidade da casa. Pizza com molho de tomate caseiro, mussarela, azeitona, pequenos pedaços de pimenta de cheiro e frango desfiado.'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Pizza de Chocolate com MM deliciosa, feita com Cobertura Chocolate Meio Amargo nestlé e nestlé Creme de Leite.'}
+];
